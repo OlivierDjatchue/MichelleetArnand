108b3200bd93d99b527bfad017e6cda0fe51ed68
OlivierDjatchue <olivierdja@yahoo.fr>
2020-10-01 08:41:29 +0200

    petites retouches

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ $(function() {
 $('.navbar-nav>li>a').on('click', function() {
 	$('.navbar-collapse').collapse('hide');
 });
+
+$(document).ready(function() {
+	var scroll_pos = 0;
+	$(document).scroll(function() {
+		scroll_pos = $(this).scrollTop();
+		if (scroll_pos > 300) {
+			$('.navbar').css('background-color', '#F85030');
+		} else {
+			$('.navbar').css('background-color', '');
+		}
+	});
+});
